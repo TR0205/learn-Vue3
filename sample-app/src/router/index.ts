fd483f4760e527a6a7635d7d90e8c7f52fa29cdb
TR0205 <index0205@gmail.com>
2022-04-26 21:10:03 +0900

    メニューコンポーネント追加

--- sample-app/src/router/index.ts
+++ sample-app/src/router/index.ts
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Top from "../views/Top.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Top",
+    component: Top,
+    meta: {
+      title: "Topページ",
+      description: "Topページの説明"
+    }
   },
   {
     path: "/about",
@@ -15,12 +19,57 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: {
+      title: "Aboutページ",
+      description: "Aboutページの説明"
+    }
+  },
+  {
+    path: "/author",
+    name: "Author",
+    component: () =>
+      import(
+        "../views/Author.vue"
+      ),
+    meta: {
+      title: "Authorページ",
+      description: "Authorページの説明"
+    }
+  },
+  {
+    path: "/production-note",
+    name: "Production Note",
+    component: () =>
+      import(
+        "../views/ProductionNote.vue"
+      ),
+    meta: {
+      title: "ProductionNoteページ",
+      description: "ProductionNoteページの説明"
+    }
+  },
+  {
+    path: "/faq",
+    name: "Q&A",
+    component: () =>
+      import(
+        "../views/Faq.vue"
+      ),
+    meta: {
+      title: "Faqページ",
+      description: "Faqページの説明"
+    }
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  // 画面遷移時にスクロール位置をトップへ戻す
+  scrollBehavior() {
+    return { left: 0, top:0 }
+  }
 });
 
-export default router;
+// メニューにはTOPへのリンクが無いためsliceで排除
+export const menuRoutes = routes.slice(1, routes.length);
